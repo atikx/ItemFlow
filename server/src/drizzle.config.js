dba788a7553fc8c4deb340e17daf9892ed13ea1b
ATIKSH09 <atikshgupta6373@gmail.com>
2025-07-19 11:11:13 +0530

    fix npm run dev issue

--- server/src/drizzle.config.js
+++ server/src/drizzle.config.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const drizzle_kit_1 = require("drizzle-kit");
 require("dotenv/config");
 exports.default = (0, drizzle_kit_1.defineConfig)({
-    schema: './src/drizzle/schema/**/*.ts',
-    out: './src/drizzle/migrations',
+    schema: './drizzle/schema/**/*.ts',
+    out: './drizzle/migrations',
     dialect: 'postgresql',
     dbCredentials: {
         url: process.env.DATABASE_URL,
