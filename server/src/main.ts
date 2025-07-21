7eb89ea82df7eeb298524e657ac63e382efa34d8
ATIKSH09 <atikshgupta6373@gmail.com>
2025-07-21 10:18:34 +0530

    add cors origin in server

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,13 +7,15 @@ config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: ['http://localhost:3000', 'https://studio.apollographql.com'],
-    credentials: true,
-  });
+app.enableCors({
+  origin: ['http://localhost:3000', 'https://item-flow.vercel.app'],
+  credentials: true,
+});
 
   app.use(cookieParser());
   const port = process.env.PORT || 8000;
+
+
   await app.listen(port);
   console.log(`Server is running on port ${port}`);
 }
